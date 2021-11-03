de0c968d41a96aefb12241d2588935cdeee7375e
Pierre <pmoretto@student.42lausanne.ch>
2021-11-03 16:13:03 +0100

    strnstr finish

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pierremoretton <pierremoretton@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/27 17:02:02 by pmoretto          #+#    #+#             */
-/*   Updated: 2021/11/03 16:02:54 by pierremoret      ###   ########.fr       */
+/*   Created: 2021/11/03 16:02:04 by pierremoret       #+#    #+#             */
+/*   Updated: 2021/11/03 16:02:33 by pierremoret      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdlib.h>
-# include <unistd.h>
+#include <libft.h>
 
-void *ft_memset( void * ptr, int val, size_t len);
-void ft_bzero(void *ptr, size_t len);
-size_t	ft_strlen(const char *c);
-void *ft_memcpy(void *dest, const void *src, size_t count);
-void *ft_memmove(void *dest, const void *src, size_t count);
-char *ft_strnstr(const char *s1, const char *s2, size_t len);
+char *strnstr(const char *s1, const char *s2, size_t len)
+{
+	size_t l2;
+
+	l2 = strlen(s2);
+	if (!l2)
+		return (char *)s1;
+	while (len >= l2) {
+		len--;
+		if (!memcmp(s1, s2, l2))
+			return (char *)s1;
+		s1++;
+	}
+	return NULL;
+}
