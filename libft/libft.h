b775dde4f0eb55b9afcdddc2649d98939266d2bf
Pierre <pmoretto@student.42lausanne.ch>
2021-11-03 15:28:31 +0100

    memcpy finish

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pierremoretton <pierremoretton@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:02:02 by pmoretto          #+#    #+#             */
-/*   Updated: 2021/11/02 21:20:04 by pierremoret      ###   ########.fr       */
+/*   Updated: 2021/11/03 15:20:35 by pierremoret      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,4 @@
 void *ft_memset( void * ptr, int val, size_t len);
 void ft_bzero(void *ptr, size_t len);
 size_t	ft_strlen(const char *c);
+void *ft_memcpy(void *dest, const void *src, size_t count);
